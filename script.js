42b8958b89ea8811bc83c40d9edd48476d70ab93
pavithramk18 <pavithram.kumaresan@gmail.com>
2023-08-07 14:51:05 +0530

    While & Do while sample programs

---
+++ script.js
@@ -0,0 +1,30 @@
+//Sample while program to print 1 to 10 
+var n=10;
+var i=1;
+while(i<n)
+{
+    console.log(i)
+    i++;
+}
+
+//Sample while Program to print 100 to 90
+
+var n=100;
+var i=90
+while(i<n)
+{
+    console.log(n);
+    n--;
+
+}
+
+//Sample do while program to print 1 to 10
+
+
+var i=0;
+do
+{
+    console.log(i)
+    i++;
+
+}while(i<10)
